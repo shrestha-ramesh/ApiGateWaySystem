da3f4514f5d90e7de6a02148732a218d470f0314
shrestha-ramesh <shresthashare@gmail.com>
2025-01-28 00:37:12 -0600

    implement filter to extract jwt token from header

---
+++ orders/src/main/java/com/orders/CustomHeaderFilter.java
@@ -0,0 +1,42 @@
+package com.orders;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+
+@Component
+public class CustomHeaderFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+//        Enumeration<String> headerNames = httpRequest.getHeaderNames();
+//        while (headerNames.hasMoreElements()) {
+//            String headerName = headerNames.nextElement();
+//            String headerValue = httpRequest.getHeader(headerName);
+//            System.out.println(headerName + ": " + headerValue);
+//        }
+        System.out.println("This is httpRequest "+httpRequest.getHeader("authorization"));
+        String authorization = httpRequest.getHeader("authorization").trim();
+        String token = getTokenFromBearerString(authorization);
+        HeaderUtil.setToken(token);
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            HeaderUtil.clear();
+        }
+    }
+    public static String getTokenFromBearerString(String authorization) {
+        int spaceIndex = authorization.indexOf(" ");
+        return spaceIndex != -1 ? authorization.substring(spaceIndex + 1) : "";
+    }
+}
