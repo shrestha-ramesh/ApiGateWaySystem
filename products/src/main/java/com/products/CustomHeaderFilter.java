1ba8ed5b5e652c062f552d05d16b3bd1cfa0a5c7
shrestha-ramesh <shresthashare@gmail.com>
2025-01-30 07:55:21 -0600

    token extract from header

---
+++ products/src/main/java/com/products/CustomHeaderFilter.java
@@ -0,0 +1,30 @@
+package com.products;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class CustomHeaderFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String authorization = httpServletRequest.getHeader("authorization").trim();
+        String token = getTokenFromBearerString(authorization);
+
+        HeaderUtil.setToken(token);
+        try {
+            chain.doFilter(request, response);
+        }finally {
+            HeaderUtil.clear();
+        }
+    }
+
+    private String getTokenFromBearerString(String authorization) {
+        int spaceIndex = authorization.indexOf(" ");
+        return spaceIndex != -1 ? authorization.substring(spaceIndex+1) : "";
+    }
+}
