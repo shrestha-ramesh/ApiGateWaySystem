782ca894b5eb7fa96e8e284e55f4c6a0d79cc31d
shrestha-ramesh <shresthashare@gmail.com>
2025-02-26 20:35:50 -0600

    implement spring security payment and products for single entry

---
+++ payment/src/main/java/com/payment/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.payment;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                        .requestMatchers(new IpAddressMatcher("127.0.0.1"))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+        ).csrf(csrf->csrf.disable());
+        return http.build();
+    }
+}
