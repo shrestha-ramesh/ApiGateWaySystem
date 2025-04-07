fa83e0463768621239c61b3866a27312c1949d0e
shrestha-ramesh <shresthashare@gmail.com>
2025-04-07 08:40:35 -0500

    create endpoints according to paymentstatus

--- payment/src/main/java/com/payment/controller/PaymentController.java
+++ payment/src/main/java/com/payment/controller/PaymentController.java
@@ -2,14 +2,53 @@ package com.payment.controller;
 
 
 import com.payment.auth.HeaderUtil;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.payment.model.PaymentRequest;
+import com.payment.service.PaymentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/payment")
 public class PaymentController {
 
-    @GetMapping("/payment")
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping("/token")
     public String getPayment(){
         return HeaderUtil.getToken();
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processPayment(@RequestBody PaymentRequest paymentRequest){
+        boolean isProcess = true;
+        String s = paymentService.handlePaymentLifecycle(paymentRequest);
+        System.out.println(s);
+
+        return isProcess ?
+                ResponseEntity.ok("Payment Service Successfully"):
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Order Failed");
+    }
+    @PutMapping("{transactionId}/complete")
+    public ResponseEntity<String> completePayment(@PathVariable int transactionId){
+        boolean isComplete = paymentService.completePayment(transactionId);
+        return ResponseEntity.ok("Complete payment");
+    }
+    @PutMapping("{transactionId}/fail")
+    public ResponseEntity<String> failPayment(@PathVariable int transactionId){
+        boolean isFail = paymentService.failPayment(transactionId);
+        return ResponseEntity.ok("Fail payment");
+    }
+
+    @PostMapping("{transactionId}/refund")
+    public ResponseEntity<String> refundPayment(@PathVariable int transactionId){
+        boolean isRefund = paymentService.refundPayment(transactionId);
+        return isRefund ?
+                ResponseEntity.ok("Payment Refund Successful") :
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Payment Refund Failed");
     }
 }
---
+++ payment/src/main/java/com/payment/model/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.payment.model;
+
+public enum PaymentStatus {
+    PENDING, COMPLETED, FAILED, REFUND
+}
